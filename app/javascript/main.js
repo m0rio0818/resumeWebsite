7d8399ca472f136fd92117811edce39e81e505e3
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-06-12 18:35:26 +0900

    レイアウト修正

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -4,12 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.getElementById('menu-icon');
     const closeIcon = document.getElementById('close-icon');
 
-
-     // クリックイベントを設定
      menuBtn.addEventListener('click', function () {
         mobileMenu.classList.toggle('hidden');
         menuIcon.classList.toggle('hidden');
         closeIcon.classList.toggle('hidden');
         console.log('Menu button clicked');
-    }, { once: true }); 
+    }) 
 });
