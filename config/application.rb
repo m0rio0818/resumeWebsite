838cdd3544b764624b7dd0cbf3c7f5160bbcee6d
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-06-13 15:24:20 +0900

    ホスト名の記載

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module ResumeWebsite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "portfolio.nakatani.blog"
   end
 end
